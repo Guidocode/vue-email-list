6f8912035b11a1682c7db2a45f5ee11f7a5c0ae3
Guidocode <guidobuono@icloud.com>
2022-05-12 13:20:30 +0200

    Miglioramento codice

--- js/script.js
+++ js/script.js
@@ -17,7 +17,12 @@ const app = new Vue({
   data: {
 
     mail: [],
-    isLoading: true
+    limitMail: 10,
+    isLoading: true,
+    urlApi: 'https://flynn.boolean.careers/exercises/api/random/mail',
+    errorMessage: '',
+    isError: false
+
   },
 
   methods: {
@@ -26,19 +31,27 @@ const app = new Vue({
     generateMail(){
 
       // Ciclo per ottenere le mail che mi servono es. 10
-      for (let i = 0; i < 10; i++) {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      for (let i = 0; i < this.limitMail; i++) {
+        axios.get(this.urlApi)
         .then(resp => {
-          // console.log(resp.data);
 
           // Pusho le 10 mail nell'array di mail 
-          this.mail.push(resp.data);
+          this.mail.push(resp.data.response);
 
           console.log(this.mail);
 
           // Una volta pushate il flag del caricamento diventa false
-          this.isLoading = false;
+          if(this.mail.length === this.limitMail){
+            this.isLoading = false;
+          }
 
+        })
+        // errore
+        .catch(error => {
+          // gli dico che il mio error message è uguale all'errore
+          this.errorMessage = error;
+          // cambio il valore della booleana in true quando c'è l'errore
+          this.isError = true;
         })
         
       };
